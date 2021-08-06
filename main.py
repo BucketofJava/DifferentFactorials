bed68d742d38b6b72a02840766ed5335bc67b0fb
BucketofJava <38705981+BucketofJava@users.noreply.github.com>
2021-08-06 07:10:09 +0000

    Added primorial

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import math
 
+# Code from: <dickinsm@gmail.com>, Nov 30 2006
+#http://groups.google.com/group/comp.lang.python/msg/f1f10ced88c68c2d
+def sieveOfEratosthenes(n):
+    """sieveOfEratosthenes(n): return the list of the primes < n."""
+    if n <= 2:
+        return []
+    sieve = range(3, n, 2)
+    top = len(sieve)
+    for si in sieve:
+        if si:
+            bottom = (si*si - 3) // 2
+            if bottom >= top:
+                break
+            sieve[bottom::si] = [0] * -((bottom - top) // si)
+    return [2] + [el for el in sieve if el]
+
 def nfactorial(num, n):
   if(num<3):
     return num;
@@ -7,6 +23,11 @@ def nfactorial(num, n):
   while num>n:
     num-=n;
     finalnum*=num;
+  return finalnum;
+def primorial(num):
+  finalnum=num;
+  for i in sieveOfEratosthenes(num):
+    finalnum*=i;
   return finalnum;
 def superFactorialPickover(num):
   tennum=math.trunc(math.log(num, 10))
@@ -24,4 +45,5 @@ def superFactorialPickover(num):
 print("\n")
 print(superFactorialPickover(30))
 print(nfactorial(45, 3))
+print(primorial(7))
 
