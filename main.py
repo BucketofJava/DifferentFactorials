633e760a221e5b959219f9b90f47f4382f4bef0e
BucketofJava <38705981+BucketofJava@users.noreply.github.com>
2021-08-06 07:30:17 +0000

    Added subfactorial and util file

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
 import math
+import util
 
-# Code from: <dickinsm@gmail.com>, Nov 30 2006
-#http://groups.google.com/group/comp.lang.python/msg/f1f10ced88c68c2d
-def sieveOfEratosthenes(n):
-    """sieveOfEratosthenes(n): return the list of the primes < n."""
-    if n <= 2:
-        return []
-    sieve = range(3, n, 2)
-    top = len(sieve)
-    for si in sieve:
-        if si:
-            bottom = (si*si - 3) // 2
-            if bottom >= top:
-                break
-            sieve[bottom::si] = [0] * -((bottom - top) // si)
-    return [2] + [el for el in sieve if el]
 
+
+#More accurate, but slower for bigger numbers
 def nfactorial(num, n):
   if(num<3):
     return num;
@@ -24,9 +12,29 @@ def nfactorial(num, n):
     num-=n;
     finalnum*=num;
   return finalnum;
+#Less accurate(Not by that much), but quicker for bigger numbers. Use n=1 for normal factorial.
+def nfactorialefficient(num, n=1):
+  if(num<3 and n!=1):
+    return num;
+  tennum=math.trunc(math.log(num, 10))
+  finalnum=num/(10**tennum);
+  while num>n:
+    num-=n;
+    finalnum*=num;
+    tennumi=math.trunc(math.log(finalnum, 10));
+    tennum+=tennumi;
+    finalnum/=(10**tennumi);
+  string="{} * 10^{}".format(finalnum, tennum)
+  return finalnum, tennum, string;
+def subfactorial(num):
+  finalnum=0;
+  for i in range(num-1):
+    finalnum+=((-1)**(i+2))*(1/math.factorial(i+2))
+  finalnum*=math.factorial(num);
+  return finalnum;
 def primorial(num):
   finalnum=num;
-  for i in sieveOfEratosthenes(num):
+  for i in util.sieveOfEratosthenes(num):
     finalnum*=i;
   return finalnum;
 def superFactorialPickover(num):
@@ -45,5 +53,7 @@ def superFactorialPickover(num):
 print("\n")
 print(superFactorialPickover(30))
 print(nfactorial(45, 3))
+print(nfactorialefficient(45, 3))
 print(primorial(7))
+print(subfactorial(5))
 
